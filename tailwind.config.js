ef7cfe1a579a4a6ccb49f409509f8ce0948bc002
zahir ali <zahirali48734@gmail.com>
2024-03-31 13:13:45 +0500

    files Added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      width:{
+        '500': '500px',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
